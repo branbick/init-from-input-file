cbece9490b59530ea7ec23eee4e7f7c81a5b1ef0
branbick <84204053+branbick@users.noreply.github.com>
2021-12-22 16:26:02 -0500

    Messing around with CMake

---
+++ test/build/CMakeFiles/tester_cpp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/branbick/git/init-from-input-file/src/init_from_input_file.c" "CMakeFiles/tester_cpp.dir/home/branbick/git/init-from-input-file/src/init_from_input_file.c.o" "gcc" "CMakeFiles/tester_cpp.dir/home/branbick/git/init-from-input-file/src/init_from_input_file.c.o.d"
+  "/home/branbick/git/init-from-input-file/src/init_tools.c" "CMakeFiles/tester_cpp.dir/home/branbick/git/init-from-input-file/src/init_tools.c.o" "gcc" "CMakeFiles/tester_cpp.dir/home/branbick/git/init-from-input-file/src/init_tools.c.o.d"
+  "/home/branbick/git/init-from-input-file/test/tester_c.c" "CMakeFiles/tester_cpp.dir/tester_c.c.o" "gcc" "CMakeFiles/tester_cpp.dir/tester_c.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
