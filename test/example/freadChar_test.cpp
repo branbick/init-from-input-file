809c2685af28f74a7c120ff0e416708062c162bb
branbick <84204053+branbick@users.noreply.github.com>
2021-12-23 16:16:11 -0500

    Improved example gtest

--- test/example/freadChar_test.cpp
+++ test/example/freadChar_test.cpp
@@ -1,23 +1,26 @@
-// Prevent C++ from name mangling the C code
+// Prevent C++ from name-mangling the C code
 extern "C" {
 #include "../../src/init_tools.h"
 }
 
 #include <gtest/gtest.h>
-#include <stdio.h>
-#include <iostream>
 
 TEST(FreadCharTest, BasicAssertions)
 {
-    FILE* pFile = fopen("../freadChar_test.inp", "r");  // TODO: Change to C++ way
+    const char* const kFileName = "../freadChar_test.inp";
+    FILE* const pFile = fopen(kFileName, "r");
     if (pFile == NULL)
-    {
-        std::cout << "Unable to open input file" << std::endl;
-        return;
-    }
+        FAIL() << "Unable to open " << kFileName << std::endl;
     
     char c;
-    freadChar(pFile, &c);
 
+    freadChar(pFile, &c);
     EXPECT_EQ(c, 'a');
+
+    freadChar(pFile, &c);
+    EXPECT_EQ(c, '\n');
+
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_FEOF);
+
+    fclose(pFile);
 }
