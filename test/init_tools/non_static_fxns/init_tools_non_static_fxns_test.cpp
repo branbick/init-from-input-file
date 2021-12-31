c2fa69c372b0086a22f27012c83cf42807e359c3
branbick <84204053+branbick@users.noreply.github.com>
2021-12-31 11:21:22 -0500

    Added more tests for stripAndLower

--- test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
+++ test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
@@ -7,13 +7,46 @@ extern "C" {
 
 #include <cstring>
 
-TEST(StripAndLowerTest, SpacesAndLowercase)
+TEST(StripAndLowerTest, NoWhiteSpaceAndCamelCase)
 {
-    const char* const kStrIn {"spaces and lowercase"};
-    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for the null terminator
+    const char* const kStrIn {"NoWhiteSpaceAndCamelCase"};
+    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for '\0'
 
     stripAndLower(kStrIn, strOut);
-    EXPECT_STREQ(strOut, "spacesandlowercase");
+    EXPECT_STREQ(strOut, "nowhitespaceandcamelcase");
+
+    delete[] strOut;
+}
+
+TEST(StripAndLowerTest, WhiteSpaceAndLowercase)
+{
+    const char* const kStrIn {"white	space and\nlowercase"};
+    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for '\0'
+
+    stripAndLower(kStrIn, strOut);
+    EXPECT_STREQ(strOut, "whitespaceandlowercase");
+
+    delete[] strOut;
+}
+
+TEST(StripAndLowerTest, WhiteSpaceAndUppercase)
+{
+    const char* const kStrIn {"WHITE	SPACE AND\nUPPERCASE"};
+    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for '\0'
+
+    stripAndLower(kStrIn, strOut);
+    EXPECT_STREQ(strOut, "whitespaceanduppercase");
+
+    delete[] strOut;
+}
+
+TEST(StripAndLowerTest, WhiteSpaceAndMixedCase)
+{
+    const char* const kStrIn {"whITe	SpacE anD\nMiXedCASe"};
+    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for '\0'
+
+    stripAndLower(kStrIn, strOut);
+    EXPECT_STREQ(strOut, "whitespaceandmixedcase");
 
     delete[] strOut;
 }
