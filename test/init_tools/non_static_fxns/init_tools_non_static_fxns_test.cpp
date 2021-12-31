402c6da69afae17f3bd432c70ed478f9b81ce2c2
branbick <84204053+branbick@users.noreply.github.com>
2021-12-30 21:12:19 -0500

    Updated unit tests for init_tools.h's non-static fxns

--- test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
+++ test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
@@ -5,34 +5,47 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
+#include <cstring>
+
+TEST(StripAndLowerTest, SpacesAndLowercase)
+{
+    const char* const kStrIn {"spaces and lowercase"};
+    char* const strOut {new char[std::strlen(kStrIn) + 1]};  // "+ 1" for the null terminator
+
+    stripAndLower(kStrIn, strOut);
+    EXPECT_STREQ(strOut, "spacesandlowercase");
+
+    delete[] strOut;
+}
+
 TEST(FreadCharTest, Comprehensive)
 {
-    const char* const kFileName = "../FreadCharTest_Comprehensive.inp";
-    FILE* const pFile = fopen(kFileName, "r");
+    const char* const kFileName {"../FreadCharTest_Comprehensive.inp"};
+    FILE* const pFile {fopen(kFileName, "r")};
     if (pFile == NULL)
         FAIL() << "Unable to open " << kFileName << std::endl;
     
     char c;
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, '!');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, '!');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, '9');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, '9');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, ' ');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, ' ');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, 'A');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, 'A');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, 'z');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, 'z');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
-    ASSERT_EQ(c, '\n');
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_ALL_GOOD);
+    EXPECT_EQ(c, '\n');
 
-    ASSERT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_FEOF);
+    EXPECT_EQ(freadChar(pFile, &c), FILE_IO_STATUS_FEOF);
 
     fclose(pFile);
 }
