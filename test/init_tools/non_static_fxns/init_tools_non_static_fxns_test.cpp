03e6ca1aef4055df49b71226d51eb0dc2f166061
branbick <84204053+branbick@users.noreply.github.com>
2022-02-19 22:23:38 -0500

    Improved PrintErrorTest

--- test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
+++ test/init_tools/non_static_fxns/init_tools_non_static_fxns_test.cpp
@@ -6,11 +6,11 @@ extern "C" {
 #include <gtest/gtest.h>
 
 #include <cstring>
+#include <cmath>
 
-TEST(PrintErrorTest, Stderr)
+TEST(PrintErrorTest, StderrBuffer)
 {
-    // TODO: Clean up (e.g., add comments and use non-literal for errMsg len)
-    const char* const kMsg {"Dummy message"};
+    const char* const kMsg {"Dummy message (ignore me)"};
     const char* const kFileMacro {__FILE__};
     const int kLineMacro {__LINE__};
     const char* const kFileName {"Dummy file name"};
@@ -20,9 +20,19 @@ TEST(PrintErrorTest, Stderr)
     setbuf(stderr, buf);
     printError(kMsg, kFileMacro, kLineMacro, kFileName, kKeyName);
 
-    char* const errMsg {new char[255]};
+    const char* const kFormatErrMsg {
+        "ERROR: %s\n       Source: %s | Line: %d | Input: %s | Key: %s\n"};
+    char* const errMsg {new char[
+        std::strlen(kFormatErrMsg) - 10  // "- 10" for the format specifiers
+        + std::strlen(kMsg)
+        + std::strlen(kFileMacro)
+        // The below is the number of digits in kLineMacro
+        + static_cast<int>(std::floor(std::log10(kLineMacro))) + 1
+        + std::strlen(kFileName)
+        + std::strlen(kKeyName)
+        + 1]};  // For the null terminator
     sprintf(errMsg,
-            "ERROR: %s\n       Source: %s | Line: %d | Input: %s | Key: %s\n",
+            kFormatErrMsg,
             kMsg, kFileMacro, kLineMacro, kFileName, kKeyName);
 
     EXPECT_STREQ(buf, errMsg);
