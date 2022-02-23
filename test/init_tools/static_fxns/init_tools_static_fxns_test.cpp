4ee06650ae7d4aa730fd612ffaf7ca69e2f0fbba
branbick <84204053+branbick@users.noreply.github.com>
2022-02-23 11:58:50 -0500

    Added first init_tools tests for STATIC fxns

--- test/init_tools/static_fxns/init_tools_static_fxns_test.cpp
+++ test/init_tools/static_fxns/init_tools_static_fxns_test.cpp
@@ -6,3 +6,30 @@ extern "C" {
 #include <gtest/gtest.h>
 
 // TODO: Add tests
+TEST(CheckStartOfLineTest, KeyFollowedBySpace)
+{
+    const char* const kFileName {
+        "../input/CheckStartOfLineTest_KeyFollowedBySpace.inp"};
+    FILE* const pFile {fopen(kFileName, "r")};
+    if (pFile == NULL)
+        FAIL() << "Unable to open " << kFileName << std::endl;
+
+    EXPECT_EQ(checkStartOfLine(pFile, "keyFollowedBySpace"),
+              FILE_IO_STATUS_KEY_FOUND);
+
+    fclose(pFile);
+}
+
+TEST(CheckStartOfLineTest, KeyFollowedByTab)
+{
+    const char* const kFileName {
+        "../input/CheckStartOfLineTest_KeyFollowedByTab.inp"};
+    FILE* const pFile {fopen(kFileName, "r")};
+    if (pFile == NULL)
+        FAIL() << "Unable to open " << kFileName << std::endl;
+
+    EXPECT_EQ(checkStartOfLine(pFile, "keyFollowedByTab"),
+              FILE_IO_STATUS_KEY_FOUND);
+
+    fclose(pFile);
+}
