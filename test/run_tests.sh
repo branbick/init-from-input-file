8da51f91bef5920fb39ae64bcfa486130e2b028e
branbick <84204053+branbick@users.noreply.github.com>
2022-03-03 11:49:56 -0500

    Updated Bash script formatting

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,11 +1,11 @@
 # !/usr/bin/bash
 
 # Names of relevant subfolders in test (level-0) folder
-# TODO: Regarding line below, uncomment '"init_from_input_file")  # Level 1'
-#       after init_from_input_file tests are "ready"
-src_files=("init_tools")  #"init_from_input_file")  # Level 1
-fxn_types=("non_static_fxns" "static_fxns")  # Level 2
-build_dir="build"  # Level 3
+# TODO: Regarding line below, uncomment "'init_from_input_file')  # Level 1"
+#       after init_from_input_file tests are written
+src_files=('init_tools')  #'init_from_input_file')  # Level 1
+fxn_types=('non_static_fxns' 'static_fxns')  # Level 2
+build_dir='build'  # Level 3
 
 # Run tests
 for src_file in "${src_files[@]}"; do
